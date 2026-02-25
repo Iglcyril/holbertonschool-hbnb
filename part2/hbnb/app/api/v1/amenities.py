3c4af648ed18073b2ee5f96ff5443558d4128b0e
Iglcyril <iglcyril@hotmail.fr>
2026-02-25 13:40:15 +0100

    feat: Pycodestyle correction and fix error on amenities update

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -7,6 +7,7 @@ amenity_model = api.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity')
 })
 
+
 @api.route('/')
 class AmenityList(Resource):
     @api.expect(amenity_model)
@@ -14,7 +15,6 @@ class AmenityList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new amenity"""
-
 
         try:
             new_amenity = facade.create_amenity(api.payload)
@@ -24,11 +24,9 @@ class AmenityList(Resource):
         except Exception as err:
             return {'error': 'Internal server error'}, 500
 
-
     @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
         """Retrieve a list of all amenities"""
-        
 
         try:
             amenities = facade.get_all_amenities()
@@ -43,7 +41,7 @@ class AmenityResource(Resource):
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
         """Get amenity details by ID"""
-        
+
         try:
             amenity = facade.get_amenity(amenity_id)
             if not amenity:
@@ -52,17 +50,15 @@ class AmenityResource(Resource):
         except Exception as err:
             return {'error': 'Internal srever error'}, 500
 
-
     @api.expect(amenity_model)
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
         """Update an amenity's information"""
-        
 
         try:
-            updated_amenity = facade.update.amenity(amenity_id, api.payload)
+            updated_amenity = facade.update_amenity(amenity_id, api.payload)
             if not updated_amenity:
                 return {'error': 'Amenity not found'}, 404
             return updated_amenity.to_dict(), 200
